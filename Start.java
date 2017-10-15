1288b6d21f64deaa9182e0811de9027fc66d4e1d
DalcaN <dcsumind@mtu.edu>
2017-10-15 14:25:20 -0400

    Removed testing comments.

--- Start.java
+++ Start.java
@@ -13,28 +13,6 @@ public class Start extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Scanner scanner = new Scanner(System.in);
-//        int option;
-//
-//        Bank bank1 = new Bank(primaryStage);
-//        bank1.loadButtons();
-//
-//        System.out.println("Choose a button to press: ");
-//        for(int i = 0; i < bank1.getButtons().length; i++) {
-//            System.out.println((i + 1) + ") " + bank1.getButton(i).toString());
-//        }
-//        option = scanner.nextInt() - 1;
-//
-//        if(option >= 0 && option <= bank1.getButtons().length) {
-//            bank1.getButton(option).press();
-//        } else {
-//            System.out.println("Not in the correct range sorry!");
-//        }
-
-
-//        for(int i = 0; i < bank1.getButtons().length; i++) {
-//            bank1.getButton(i).press();
-//        }
 
         Launchpad launchpad = new Launchpad(primaryStage);
         launchpad.loadBanks();
